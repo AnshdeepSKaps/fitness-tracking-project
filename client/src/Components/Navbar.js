35bfede010b68528a723567c8ded121e2b5f1b63
anshdeep <kapulaanshdeep@gmail.com>
2022-11-24 20:53:49 +0530

    somethign

--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -12,6 +12,12 @@ export default function Navbar() {
     color: "white"
   }
 
+  const name1Style = {
+    fontSize: "1.5rem",
+    marginLeft: "2vw",
+    color: "white"
+  }
+
   const navStyle = {
     // backgroundColor: "linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.9)) !important",
     backgroundColor: "rgba(50,50,50,50.5)",
@@ -19,13 +25,14 @@ export default function Navbar() {
   }
 
   return (
-
     <nav>
       <div style={navStyle} className="container-fluid d-flex align-items-center">
         <Link to="../">
           <img style={imageStyle} src="https://i.postimg.cc/3R2b0grK/logo-removebg-preview-1.png" alt="" />
         </Link>
         <Link style={nameStyle} className="btn" to="../">FiTrack</Link>
+        <Link style={name1Style} to="/login">Login</Link>
+        <Link style={name1Style} to="/reg">Registration</Link>
         <div style={{
           position: "relative",
           top: "0",
