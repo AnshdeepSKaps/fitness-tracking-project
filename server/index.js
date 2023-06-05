e9419d081cceb6d68d075846ff3294c0af4859c8
anshdeep <kapulaanshdeep@gmail.com>
2023-06-05 23:46:18 +0530

    auth

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ const app = express()
 app.use(express.json())
 
 const corsOptions = {
-    origin: "http://localhost:3000",
+    origin: "https://dapper-capybara-688970.netlify.app/",
     credentials: true,            //access-control-allow-credentials:true
     optionSuccessStatus: 200,
     methods: "GET,POST,OPTIONS"
