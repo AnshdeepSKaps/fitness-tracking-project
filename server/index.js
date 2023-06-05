9a0c2135cc8a928355a5f89182f7412858404646
anshdeep <kapulaanshdeep@gmail.com>
2023-06-06 01:39:52 +0530

    atuh

--- server/index.js
+++ server/index.js
@@ -24,24 +24,10 @@ const corsOptions = {
     methods: "GET,POST,OPTIONS"
 }
 
-app.use(function (req, res, next) {
+// app.use(function (req, res, next) {
 
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://dapper-capybara-688970.netlify.app');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type', 'Content-Type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
+    
+// });
 
 app.use(cors(corsOptions))
 app.options('https://dapper-capybara-688970.netlify.app', cors(corsOptions))
